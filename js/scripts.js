6cf46fdb5e750d30e0d4ece0bcba2316aadc3472
Kimberly Bordon <kbordon@gmail.com>
2017-09-27 19:49:31 -0700

    Hilight score depending on current player.

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ Player.prototype.rollDie = function() {
     this.turnScore = this.turnScore + rollResult;
     if (this.totalScore + this.turnScore >= 100) {
       this.totalScore = this.totalScore + this.turnScore;
-      return "WINNER!";
+      return "win";
     }
     return rollResult;
 
@@ -33,52 +33,68 @@ $(document).ready(function(){
     // var nameInput = $("#player-one").val();
     // var newPlayer = new Player(nameInput);
     var pigSound = new Audio('quiet_pig.mp3');
-    $("#create-player").hide();
+    $("#create-players").hide();
     $("#button-play").show();
+
     var playerArray = [];
     $("input").each(function() {
       var newPlayer = new Player($(this).val());
       playerArray.push(newPlayer);
     })
 
-    playerArray.forEach(function(player) {
-      $("#score-keeper").append("<div class='player-score'>" + player.playerName + "<br><span class='" + player.playerName + "-total'>" + 0 + "</span></div>");
-    });
+    // playerArray.forEach(function(player) {
+    //   $("#score-keeper").append("<div class='player-score'>" + player.playerName + "<br><span class='" + player.playerName + "-total'>" + 0 + "</span></div>");
+    // });
+
+    // for ( var playersIndex = 0; playersIndex < playerArray.length; playersIndex++) {
+    //   $("#score-keeper").append("<div class='player-" + playersIndex + "'>" + playerArray[playersIndex].playerName + "<br><span class='total-" + playersIndex + "'>" + 0 + "</span></div>");
+    // }
+
+    for ( var playersIndex = 0; playersIndex < playerArray.length; playersIndex++) {
+      $("#score-keeper").append("<li class='player-" + playersIndex + "'>" + playerArray[playersIndex].playerName + "<br><span class='total-" + playersIndex + "'>" + 0 + "</span></li>");
+    }
+
 
     var currentPlayerIndex = 0;
 
     $("button#button-play").click(function () {
-      $("." + playerArray[currentPlayerIndex].playerName + "-total").addClass("score-current");
-      var notOne = playerArray[currentPlayerIndex].rollDie();
+      $(".player-" + currentPlayerIndex).addClass("score-current");
+      var dieResult = playerArray[currentPlayerIndex].rollDie();
       // $("#score-total").text(playerArray[currentPlayerIndex].playerName + " text ");
-      if (notOne === "WINNER!") {
+      if (dieResult === "win") {
         $("#button-play, #button-hold").hide();
-        $("#score-total").text(playerArray[currentPlayerIndex].playerName + " WINS! Your Score is " + playerArray[currentPlayerIndex].totalScore);
-      } else if (notOne) {
-        $("#score-total").text(playerArray[currentPlayerIndex].playerName + " rolled a " + notOne  + ". Turn Score: " + playerArray[currentPlayerIndex].turnScore);
+        $("#score-total").text(playerArray[currentPlayerIndex].playerName + " WINS! Your Score is " + playerArray[currentPlayerIndex].totalScore + "!");
+      } else if (dieResult) {
+        $("#score-total").text(playerArray[currentPlayerIndex].playerName + " rolled a " + dieResult  + ". Turn Score: " + playerArray[currentPlayerIndex].turnScore);
         $("button#button-hold").show();
       } else {
         $("#button-hold").hide();
-        $("#score-total").text(playerArray[currentPlayerIndex].playerName + " rolled a 1, and lost their turn! Next player!")
+        $(".player-" + currentPlayerIndex).removeClass("score-current");
+        $("#score-total").text(playerArray[currentPlayerIndex].playerName + " rolled a 1, and lost their turn!")
         pigSound.play();
         if ((playerArray.length - 1) === currentPlayerIndex) {
           currentPlayerIndex = 0;
         } else {
           currentPlayerIndex++;
         }
+        $(".player-" + currentPlayerIndex).addClass("score-current");
+        $("#score-total").append(" It's " + playerArray[currentPlayerIndex].playerName + "'s turn.");
       }
 
     });
 
     $("button#button-hold").click(function () {
       playerArray[currentPlayerIndex].holdDie();
+      $(".player-" + currentPlayerIndex).removeClass("score-current");
       $("#score-total").text(playerArray[currentPlayerIndex].playerName + " held.");
-      $("." + playerArray[currentPlayerIndex].playerName + "-total").text(playerArray[currentPlayerIndex].totalScore);
+      $(".total-" + currentPlayerIndex).text(playerArray[currentPlayerIndex].totalScore);
       if ((playerArray.length - 1) === currentPlayerIndex) {
         currentPlayerIndex = 0;
       } else {
         currentPlayerIndex++;
       }
+      $(".player-" + currentPlayerIndex).addClass("score-current");
+      $("#score-total").append(" It's " + playerArray[currentPlayerIndex].playerName + "'s turn.");
     })
   })
 
